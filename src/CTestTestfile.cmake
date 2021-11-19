824d3fa1aabf68c31ab81e03c60ba0edefe6d672
233timmy228 <68225262+233timmy228@users.noreply.github.com>
2021-11-20 00:39:51 +0800

    Add files via upload

---
+++ src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ganl/DB/scudb_initial/src
+# Build directory: /home/ganl/DB/scudb_initial/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
